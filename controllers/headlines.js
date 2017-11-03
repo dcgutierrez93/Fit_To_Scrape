eb3321c400de1c571dc46846b378fcd6780725be
David Gutierrez <dcgutierrez93@yahoo.com>
2017-11-03 16:06:11 -0500

    added to Headline.js

--- controllers/headlines.js
+++ controllers/headlines.js
@@ -5,3 +5,30 @@
 // =============================================================================
 var scrape   = require('../scripts/scrape');
 var makeDate = require('../scripts/date');
+
+
+var Headline = require("../models/Headline");
+
+module.exports = {
+    fetch: function(cb) {
+        // Run Scrape function
+        scrape(function(data) {
+            var articles = data;
+
+            for (var i = 0; i < articles.length; i++) {
+                articles[i].date  = makeDate();
+                articles[i].saved = false;
+            }
+            Headline.collection.insertMany(articles, { ordered: false }, function(err, docs) {
+                cb(err, docs);
+            });
+        });
+    },
+    delete: function(query, cb) {
+        Headline.remove(query, cb);
+    },
+    get: function(query, cb) {
+        Headline.find(query)
+            
+    }
+}
