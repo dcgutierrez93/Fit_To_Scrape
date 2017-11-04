338eeae10085158d060019893bea976731d18a0f
dcgutierrez93 <dcgutierrez93@yahoo.com>
2017-11-04 11:26:59 -0500

    Cleaned Model

--- models/Headline.js
+++ models/Headline.js
@@ -1,36 +1,29 @@
-// *********************
+// =============================================================================
+
 // Headline Model
-// *********************
 
-// Require mongoose
+// =============================================================================
 var mongoose = require('mongoose');
-
-// Create Schema
-var Schema = mongoose.Schema;
+var Schema   = mongoose.Schema;
 
 // Create the Headline Schema
 var headlineSchema = new Schema ({
-  // headline string must be entered.
   headline: {
-    type: String,
+    type    : String,
     required: true,
-    unique: true
+    unique  : true
   },
-  // Sumary string must be entered.
   summary: {
-    type: String,
+    type    : String,
     required: true,
   },
-  // Date is just a string.
   date: String,
   saved: {
-    type: Boolean,
+    type   : Boolean,
     default: false
   }
 });
 
-// Create our headline Model
 var Headline = mongoose.model("Headline", headlineSchema);
 
-// Export Headline
 module.exports = Headline;
